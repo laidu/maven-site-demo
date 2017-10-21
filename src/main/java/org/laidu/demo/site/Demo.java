75558847d021b492c265e3a38bec2dc87aa944dd
laidu <laidu823@gmail.com>
2017-10-21 14:29:33 +0800

    first commit

---
+++ src/main/java/org/laidu/demo/site/Demo.java
@@ -0,0 +1,14 @@
+package org.laidu.demo.site;
+
+
+/**
+ * demo
+ * maven site demo
+ * Created by tiancai.zang on 2017-10-20 23:24.
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
